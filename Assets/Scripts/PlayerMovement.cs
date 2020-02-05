db3e2873afe6a90535c3488992c6151e0d94e40d
Betonson <leonel3289@mail.ru>
2020-02-05 22:12:26 +0300

    Adding the actual project
    This is the starting point. Movement is done.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    [Range(0, 100f)][SerializeField] public float runSpeed = 40f;
+
+    float horisontalMove = 0f;
+
+    bool jump = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        horisontalMove = Input.GetAxisRaw("Horisontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        controller.Move(horisontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
